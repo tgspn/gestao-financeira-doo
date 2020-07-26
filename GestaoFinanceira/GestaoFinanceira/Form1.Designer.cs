d6d2e2c7f948fc83e7c27a8c6ef77b23bc48f764
Messias Oliveira <messias.oliveira6296@gmail.com>
2020-07-26 16:59:54 -0300

    V0 DashBoard

--- GestaoFinanceira/GestaoFinanceira/Form1.Designer.cs
+++ GestaoFinanceira/GestaoFinanceira/Form1.Designer.cs
@@ -1,6 +1,6 @@
 namespace GestaoFinanceira
 {
-    partial class Form1
+    partial class frm_DashBoard
     {
         /// <summary>
         /// Required designer variable.
@@ -31,13 +31,218 @@ namespace GestaoFinanceira
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lb_Balance = new System.Windows.Forms.Label();
+            this.lb_Revenue = new System.Windows.Forms.Label();
+            this.lb_Expense = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.opçõesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gerarRelatórioToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sairToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Saldo em Contas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(102, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Receitas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(260, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Despesas";
+            // 
+            // lb_Balance
+            // 
+            this.lb_Balance.AutoSize = true;
+            this.lb_Balance.Location = new System.Drawing.Point(167, 57);
+            this.lb_Balance.Name = "lb_Balance";
+            this.lb_Balance.Size = new System.Drawing.Size(45, 13);
+            this.lb_Balance.TabIndex = 0;
+            this.lb_Balance.Text = "R$ 0,00";
+            // 
+            // lb_Revenue
+            // 
+            this.lb_Revenue.AutoSize = true;
+            this.lb_Revenue.Location = new System.Drawing.Point(102, 112);
+            this.lb_Revenue.Name = "lb_Revenue";
+            this.lb_Revenue.Size = new System.Drawing.Size(45, 13);
+            this.lb_Revenue.TabIndex = 0;
+            this.lb_Revenue.Text = "R$ 0,00";
+            // 
+            // lb_Expense
+            // 
+            this.lb_Expense.AutoSize = true;
+            this.lb_Expense.Location = new System.Drawing.Point(269, 112);
+            this.lb_Expense.Name = "lb_Expense";
+            this.lb_Expense.Size = new System.Drawing.Size(45, 13);
+            this.lb_Expense.TabIndex = 0;
+            this.lb_Expense.Text = "R$ 0,00";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.opçõesToolStripMenuItem,
+            this.sobreToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(382, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // opçõesToolStripMenuItem
+            // 
+            this.opçõesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gerarRelatórioToolStripMenuItem,
+            this.sairToolStripMenuItem});
+            this.opçõesToolStripMenuItem.Name = "opçõesToolStripMenuItem";
+            this.opçõesToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.opçõesToolStripMenuItem.Text = "Opções";
+            // 
+            // gerarRelatórioToolStripMenuItem
+            // 
+            this.gerarRelatórioToolStripMenuItem.Name = "gerarRelatórioToolStripMenuItem";
+            this.gerarRelatórioToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.gerarRelatórioToolStripMenuItem.Text = "Gerar Relatório";
+            // 
+            // sairToolStripMenuItem
+            // 
+            this.sairToolStripMenuItem.Name = "sairToolStripMenuItem";
+            this.sairToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sairToolStripMenuItem.Text = "Sair";
+            this.sairToolStripMenuItem.Click += new System.EventHandler(this.sairToolStripMenuItem_Click);
+            // 
+            // sobreToolStripMenuItem
+            // 
+            this.sobreToolStripMenuItem.Name = "sobreToolStripMenuItem";
+            this.sobreToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.sobreToolStripMenuItem.Text = "Sobre";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::GestaoFinanceira.Properties.Resources.icons8_plus_30px;
+            this.pictureBox4.Location = new System.Drawing.Point(170, 194);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(31, 36);
+            this.pictureBox4.TabIndex = 1;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::GestaoFinanceira.Properties.Resources.CreditCard;
+            this.pictureBox6.Location = new System.Drawing.Point(320, 194);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(50, 47);
+            this.pictureBox6.TabIndex = 1;
+            this.pictureBox6.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::GestaoFinanceira.Properties.Resources.down;
+            this.pictureBox2.Location = new System.Drawing.Point(228, 89);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(24, 36);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::GestaoFinanceira.Properties.Resources.BankAccount;
+            this.pictureBox5.Location = new System.Drawing.Point(12, 194);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(39, 47);
+            this.pictureBox5.TabIndex = 1;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::GestaoFinanceira.Properties.Resources.up;
+            this.pictureBox1.Location = new System.Drawing.Point(72, 89);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(24, 36);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frm_DashBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(382, 242);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lb_Expense);
+            this.Controls.Add(this.lb_Revenue);
+            this.Controls.Add(this.lb_Balance);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frm_DashBoard";
+            this.Text = "Gestão Financeira";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lb_Balance;
+        private System.Windows.Forms.Label lb_Revenue;
+        private System.Windows.Forms.Label lb_Expense;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem opçõesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gerarRelatórioToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sairToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreToolStripMenuItem;
     }
 }
 
--- GestaoFinanceira/GestaoFinanceira/Form1.cs
+++ GestaoFinanceira/GestaoFinanceira/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace GestaoFinanceira
 {
-    public partial class Form1 : Form
+    public partial class frm_DashBoard : Form
     {
-        public Form1()
+        public frm_DashBoard()
         {
             InitializeComponent();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
