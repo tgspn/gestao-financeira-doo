0878d3808172c575c9ae480ba0216a914fab4ca4
Messias Oliveira <messias.oliveira6296@gmail.com>
2020-07-26 19:05:36 -0300

    #2: commit ajuste

--- GestaoFinanceira/GestaoFinanceira/Views/frm_DashBoard.cs
+++ GestaoFinanceira/GestaoFinanceira/Views/frm_DashBoard.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GestaoFinanceira.Views;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,11 +16,18 @@ namespace GestaoFinanceira
         public frm_DashBoard()
         {
             InitializeComponent();
+
         }
 
         private void sairToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void lb_MesAtual_Click(object sender, EventArgs e)
+        {
+            Form form = new frm_MothCalendar();
+            form.Show();
         }
     }
 }
