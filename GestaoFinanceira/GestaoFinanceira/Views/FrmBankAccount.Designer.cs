ed175bdf78f101053c01453ca13430402272990e
Messias Oliveira <messias.oliveira6296@gmail.com>
2020-07-28 00:17:38 -0300

    #6: Teste Commit

--- GestaoFinanceira/GestaoFinanceira/Views/FrmBankAccount.Designer.cs
+++ GestaoFinanceira/GestaoFinanceira/Views/FrmBankAccount.Designer.cs
@@ -28,7 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(535, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // FrmBankAccount
             // 
@@ -36,6 +46,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Name = "FrmBankAccount";
             this.Text = "FrmBankAccount";
             this.ResumeLayout(false);
@@ -43,5 +54,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
