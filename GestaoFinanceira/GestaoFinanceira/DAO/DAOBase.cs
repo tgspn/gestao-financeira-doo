3f359533702f2e5805f8a82e2772036d9cc20ae3
Tiago <tiago.spana@gmail.com>
2020-08-06 19:39:14 -0300

    feat (DAO) : generated DOO base

---
+++ GestaoFinanceira/GestaoFinanceira/DAO/DAOBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoFinanceira.DAO
+{
+    public abstract class DAOBase<TModel>
+    {
+        public abstract List<TModel> Get();
+        public abstract TModel Update();
+        public abstract TModel Insert();
+        public abstract TModel Delete();
+
+    }
+}
