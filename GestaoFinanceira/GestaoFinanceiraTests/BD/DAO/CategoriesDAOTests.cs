b350f2c6555bcafed5fde4ce9f4698f23dae2dc0
Tiago <tiago.spana@gmail.com>
2020-08-06 20:53:39 -0300

    test (DAO) : Created test for Categories DAO

---
+++ GestaoFinanceira/GestaoFinanceiraTests/BD/DAO/CategoriesDAOTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GestaoFinanceira.BD.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoFinanceira.BD.DAO.Tests
+{
+    [TestClass()]
+    public class CategoriesDAOTests
+    {
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            var categoria = new CategoriesDAO();
+            Assert.ThrowsException<NotImplementedException>(() =>
+            {
+                categoria.Delete();
+            });
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            var categoria = new CategoriesDAO();
+            Assert.ThrowsException<NotImplementedException>(() =>
+            {
+                categoria.Get();
+            });
+        }
+
+        [TestMethod()]
+        public void InsertTest()
+        {
+            var categoria = new CategoriesDAO();
+            Assert.ThrowsException<NotImplementedException>(() =>
+            {
+                categoria.Insert();
+            });
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            var categoria = new CategoriesDAO();
+            Assert.ThrowsException<NotImplementedException>(() =>
+            {
+                categoria.Update();
+            });
+        }
+    }
+}
