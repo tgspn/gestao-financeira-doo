a2ea6eb0768dc873f70850d94597e4d2221c4c34
Tiago <tiago.spana@gmail.com>
2020-08-06 19:39:51 -0300

    feat (DAO): created DAO Sample

---
+++ GestaoFinanceira/GestaoFinanceira/DAO/CategoriesDAO.cs
@@ -0,0 +1,32 @@
+﻿using GestaoFinanceira.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoFinanceira.DAO
+{
+    public class CategoriesDAO : DAOBase<Categories>
+    {
+        public override Categories Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override List<Categories> Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Categories Insert()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Categories Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
