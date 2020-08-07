fb2e2ed19dcfa4979043cc31e0bdcff91470840c
Tiago <tiago.spana@gmail.com>
2020-08-07 11:15:21 -0300

    feat (DAO) : include Entry DAO

---
+++ GestaoFinanceira/GestaoFinanceira/BD/DAO/EntryExpensesDAO.cs
@@ -0,0 +1,46 @@
+﻿using GestaoFinanceira.BD.Conections;
+using GestaoFinanceira.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoFinanceira.BD.DAO
+{
+    public class EntryExpensesDAO : DAOBase<EntryExpenses>
+    {
+        public EntryExpensesDAO(IConnection<EntryExpenses> connection):base(connection)
+        {
+
+        }
+
+        public override bool Delete(int id)
+        {
+            return connection.Delete(id);
+        }
+
+        public override IEnumerable<EntryExpenses> Get()
+        {
+            return connection.Select();
+        }
+
+        public override EntryExpenses Insert(EntryExpenses expenses)
+        {
+            var id=connection.Insert(expenses);
+            expenses.Id = id;
+            return expenses;
+        }
+
+        public override EntryExpenses Update(EntryExpenses expenses)
+        {
+            connection.Update(expenses);
+            return expenses;
+        }
+
+        protected override string GetTableName()
+        {
+            return "expenses";
+        }
+    }
+}
---
+++ GestaoFinanceira/GestaoFinanceira/Controllers/EntryExpensesController.cs
@@ -0,0 +1,42 @@
+﻿using GestaoFinanceira.BD.Conections;
+using GestaoFinanceira.BD.DAO;
+using GestaoFinanceira.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestaoFinanceira.Controllers
+{
+    public class EntryExpensesController
+    {
+        private EntryExpensesDAO dao;
+        private IConnection<EntryExpenses> connection;
+        public EntryExpensesController(IConnection<EntryExpenses> connection)
+        {
+            this.connection = connection;
+            this.dao = new EntryExpensesDAO(connection);
+        }
+        public List<EntryExpenses> List()
+        {
+            return dao.Get().ToList();
+        }
+        public EntryExpenses Find(int entryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(EntryExpenses entry)
+        {
+            if (entry.Id == 0)
+                dao.Insert(entry);
+            else
+                dao.Update(entry);
+        }
+        public void Remove(EntryExpenses entry)
+        {
+            dao.Delete(entry.Id);
+        }
+    }
+}
