d9a3395746ff88f48594a8f5934332db7c6c962d
Tiago <tiago.spana@gmail.com>
2020-08-17 12:55:57 -0300

    fix(EntryExpenses) : defined empty contructor

--- GestaoFinanceira/GestaoFinanceira/Model/EntryExpenses.cs
+++ GestaoFinanceira/GestaoFinanceira/Model/EntryExpenses.cs
@@ -5,20 +5,10 @@ namespace GestaoFinanceira.Model
 {
     public class EntryExpenses
     {
-        public int Id { get; set; }
-        public string Description { get; set; }
-        public double Value { get; set; }
-        public DateTime Date { get; set; }
-        public bool Status { get; set; }
-        public Categories Category { get; set; } = new Categories();
-        public SubCategories SubCategory { get; set; } = new SubCategories();
-        public PaymentMethod PaymentMethod { get; set; }
-        public string CaptionCategories { get; set; }
-        public string CaptionRepeat { get; set; }
-        public bool Repeat { set; get; }
-        public DateTime RepeatUntil { get; set; }
-        public EntryType EntryType { get; set; }
+        public EntryExpenses()
+        {
 
+        }
         public EntryExpenses(string description, double value, DateTime date, bool status, Categories categorie, SubCategories subCategorie, PaymentMethod paymentMethod, bool repeat, DateTime repeatUntil, EntryType entryType)
         {
             Description = description;
@@ -34,6 +24,21 @@ namespace GestaoFinanceira.Model
             this.SetDescriptionCategories();
             this.SetRepeat();
         }
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public double Value { get; set; }
+        public DateTime Date { get; set; }
+        public bool Status { get; set; }
+        public Categories Categorie { get; set; } = new Categories();
+        public SubCategories SubCategorie { get; set; } = new SubCategories();
+        public PaymentMethod PaymentMethod { get; set; }
+        public string CaptionCategories { get; set; }
+        public string CaptionRepeat { get; set; }
+        public bool Repeat { set; get; }
+        public DateTime RepeatUntil { get; set; }
+        public EntryType EntryType { get; set; }
+
+        
 
         public void SetDescriptionCategories()
         {
