1303dfa33335751f3e06670af45fc61ee5ba7c84
Tiago <tiago.spana@gmail.com>
2020-08-18 01:43:58 -0300

    feat (migration) : try migration

---
+++ GestaoFinanceira/GestaoFinanceira/Migrations/Configuration.cs
@@ -0,0 +1,25 @@
+﻿namespace GestaoFinanceira.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<GestaoFinanceira.BD.Conections.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+            ContextKey = "GestaoFinanceira.BD.Conections.ApplicationDbContext";
+        }
+
+        protected override void Seed(GestaoFinanceira.BD.Conections.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
