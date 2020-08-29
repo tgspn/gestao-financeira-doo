c21dbe72dfa2330ce9475546a130c221f66df8f2
Tiago <tiago.spana@gmail.com>
2020-08-29 11:52:39 -0300

    feat (export) : all features

--- GestaoFinanceira/GestaoFinanceira/Controllers/ReportExportImportManager.cs
+++ GestaoFinanceira/GestaoFinanceira/Controllers/ReportExportImportManager.cs
@@ -22,15 +22,102 @@ namespace GestaoFinanceira.Controllers
             using (var pack = new ExcelPackage())
             {
                 SetTabIndicadores(pack.Workbook, report);
-                SetTabReport(pack.Workbook, report);
-
+                SetTabExpenses(pack.Workbook, report.EntryExpenses,"Despesas");
+                SetTabExpenses(pack.Workbook, report.EntryRevenue,"Receitas");
+                SetTabAccount(pack.Workbook, report.Accounts);
+                SetTabCreditCards(pack.Workbook, report.CreditCards);
                 pack.SaveAs(new System.IO.FileInfo(filename));
 
             }
         }
-        private static void SetTabReport(ExcelWorkbook workbook, Report report)
+
+        private static void SetTabCreditCards(ExcelWorkbook workbook, List<CreditCard> creditCards)
         {
-            var ws = workbook.Worksheets.Add("Relatório");
+            var ws = workbook.Worksheets.Add("Cartão de Crédido");
+
+            ws.Cells["A1:F1"].Style.Border.Bottom.Style =
+                ws.Cells["F1"].Style.Border.Right.Style = ExcelBorderStyle.Thick;
+            ws.Cells["A1:F1"].Style.Font.Bold = true;
+            ws.View.ShowGridLines = false;
+            ws.DefaultColWidth = 40;
+
+            ws.Cells["A1"].Value = "Banco";
+            ws.Cells["B1"].Value = "Titular";
+            ws.Cells["C1"].Value = "Número do cartão";
+            ws.Cells["D1"].Value = "Expira em";
+            ws.Cells["E1"].Value = "Fechamento";
+            ws.Cells["F1"].Value = "Limite";
+            ws.Cells["G1"].Value = "Saldo";
+            ws.Cells["h1"].Value = "Taxa";
+
+
+            for (int i = 0, row = 2; i < creditCards.Count; i++, row++)
+            {
+                var item = creditCards[i];
+
+                ws.Cells[$"A{row}"].Value = item.Issuer;
+                ws.Cells[$"B{row}"].Value = item.Holder;
+                ws.Cells[$"C{row}"].Value = item.Number;
+                ws.Cells[$"D{row}"].Value = item.ExpirationDate;
+                ws.Cells[$"E{row}"].Value = item.ClosingDate;
+                ws.Cells[$"F{row}"].Value = item.Limit.ToString();
+                ws.Cells[$"G{row}"].Value = item.Amount.ToString();
+                ws.Cells[$"H{row}"].Value = item.LateFee.ToString()+"%";
+
+                ws.Cells[$"F{row}"].Style.Numberformat.Format =
+                ws.Cells[$"G{row}"].Style.Numberformat.Format = currencyFormat;
+            }
+
+            var range = ws.Cells[$"A1:H{creditCards.Count + 1}"];
+            range.Style.Border.Top.Style =
+                range.Style.Border.Left.Style =
+                range.Style.Border.Bottom.Style =
+                range.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+        }
+
+        private static void SetTabAccount(ExcelWorkbook workbook, List<Account> accounts)
+        {
+            var ws = workbook.Worksheets.Add("Conta Bancária");
+
+            ws.Cells["A1:F1"].Style.Border.Bottom.Style =
+                ws.Cells["F1"].Style.Border.Right.Style = ExcelBorderStyle.Thick;
+            ws.Cells["A1:F1"].Style.Font.Bold = true;
+            ws.View.ShowGridLines = false;
+            ws.DefaultColWidth = 40;
+
+            ws.Cells["A1"].Value = "Banco";
+            ws.Cells["B1"].Value = "Agência";
+            ws.Cells["C1"].Value = "Número da conta";
+            ws.Cells["D1"].Value = "Titular";
+            ws.Cells["E1"].Value = "Limite";
+            ws.Cells["F1"].Value = "Saldo";
+
+
+            for (int i = 0, row = 2; i < accounts.Count; i++, row++)
+            {
+                var item = accounts[i];
+
+                ws.Cells[$"A{row}"].Value = item.Bank;
+                ws.Cells[$"B{row}"].Value = item.Agency;
+                ws.Cells[$"C{row}"].Value = item.AccountBank;
+                ws.Cells[$"D{row}"].Value = item.Holder;
+                ws.Cells[$"E{row}"].Value = item.Limit.ToString();
+                ws.Cells[$"F{row}"].Value = item.Balance.ToString();
+
+                ws.Cells[$"E{row}"].Style.Numberformat.Format =
+                ws.Cells[$"F{row}"].Style.Numberformat.Format = currencyFormat;
+            }
+
+            var range = ws.Cells[$"A1:F{accounts.Count + 1}"];
+            range.Style.Border.Top.Style =
+                range.Style.Border.Left.Style =
+                range.Style.Border.Bottom.Style =
+                range.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+        }
+
+        private static void SetTabExpenses(ExcelWorkbook workbook, List<EntryExpenses> entries,string title)
+        {
+            var ws = workbook.Worksheets.Add(title);
 
             ws.Cells["A1:E1"].Style.Border.Bottom.Style =
                 ws.Cells["E1"].Style.Border.Right.Style = ExcelBorderStyle.Thick;
@@ -45,9 +132,9 @@ namespace GestaoFinanceira.Controllers
             ws.Cells["E1"].Value = "Pagamento";
 
 
-            for (int i = 0, row = 2; i < report.EntryExpenses.Count; i++, row++)
+            for (int i = 0, row = 2; i < entries.Count; i++, row++)
             {
-                var entry = report.EntryExpenses[i];
+                var entry = entries[i];
 
                 ws.Cells[$"A{row}"].Value = entry.Description;
                 ws.Cells[$"B{row}"].Value = entry.EntryType == EntryType.Expense ? entry.Value * -1 : entry.Value;
@@ -59,7 +146,7 @@ namespace GestaoFinanceira.Controllers
                 ws.Cells[$"C{row}"].Style.Numberformat.Format = "dd/mm/yyyy";
             }
 
-            var range = ws.Cells[$"A1:E{report.EntryExpenses.Count + 1}"];
+            var range = ws.Cells[$"A1:E{entries.Count + 1}"];
             range.Style.Border.Top.Style =
                 range.Style.Border.Left.Style =
                 range.Style.Border.Bottom.Style =
